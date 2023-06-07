0bff2babf97c6c3aadd596db0e3dc0f704f0a391
kousik <101384882+Speed46@users.noreply.github.com>
2023-06-07 13:04:55 +0530

    Update Home2.js

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -24,7 +24,7 @@ function Home2() {
               <br />
               <br />I am fluent in classics like
               <i>
-                <b className="purple"> ReactJS, Javascrip and Java. </b>
+                <b className="purple"> ReactJS, Javascript and Java. </b>
               </i>
               <br />
               <br />
